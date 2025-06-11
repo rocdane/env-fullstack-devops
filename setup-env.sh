433bf682fb13d5716611277486133746ee1bc5cd
rochdane <48872607+rocdane@users.noreply.github.com>
2025-06-11 00:38:53 +0000

    Update setup-env.sh
    replace php-intl by php8.3-intl

--- setup-env.sh
+++ setup-env.sh
@@ -17,10 +17,10 @@ sudo apt install -y build-essential curl wget git unzip gnupg ca-certificates ss
 echo "🐘 Installation de PHP 8.3 et extensions..."
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
-sudo apt install -y php8.3 php8.3-cli php8.3-fpm php8.3-common php8.3-mbstring php8.3-xml php8.3-mysql php8.3-curl php8.3-zip php8.3-bcmath php8.3-tokenizer php8.3-gd
+sudo apt install -y php8.3 php8.3-cli php8.3-fpm php8.3-common php8.3-mbstring php8.3-xml php8.3-mysql php8.3-curl php8.3-zip php8.3-bcmath php8.3-tokenizer php8.3-gd php8.3-intl
 
 echo "📦 Installation de Composer et Laravel..."
-sudo apt install -y composer php-intl
+sudo apt install -y composer
 composer global require laravel/installer
 echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> ~/.bashrc
 
